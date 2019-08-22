8046f1ea97b27509b31664750d1bb1ffe0ee77b4
matteonoccodev <52407199+matteonoccodev@users.noreply.github.com>
2019-08-22 09:55:31 +0200

    Creazione Controller ClosePointsController

---
+++ src/DomainModel/Classes/CriteriDiRicerca.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.Classes
+{
+    public class CriteriDiRicerca
+    {
+        /// <summary>
+        /// Rappresenta la latitudine del punto da utilizzare per la ricerca
+        /// </summary>
+        public double Latitudine { get; set; }
+
+        /// <summary>
+        /// Rappresenta la longitudine del punto da utilizzare per la ricerca
+        /// </summary>
+        public double Longitudine { get; set; }
+
+        /// <summary>
+        /// Rappresenta la distanza nella quale devono essere ricercati i punti a partire dalle coordinate immesse
+        /// </summary>
+        public double Distance { get; set; }
+
+        /// <summary>
+        /// Rappresenta la pagina da visuallizare contenente i punti ottenuti tramite la ricerca ricerca
+        /// </summary>
+        public int Page { get; set; }
+    }
+}
---
+++ src/RockApi/Controllers/ClosePointsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Queries;
+using DomainModel.Classes;
+using DomainModel.CQRS.Queries.GetClosePoints;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RockApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClosePointsController : ControllerBase
+    {
+        public IQueryHandler<GetClosePointsQuery, GetClosePointsQueryResult> handler { get; }
+
+
+        public ClosePointsController (IQueryHandler <GetClosePointsQuery, GetClosePointsQueryResult> handler)
+        {
+            this.handler = handler;
+        }
+
+
+        [HttpGet]
+        public ActionResult<GetClosePointsQueryResult> Get ([FromQuery] CriteriDiRicerca criteri)
+        {
+            var query = new GetClosePointsQuery()
+            {
+                Latitudine = criteri.Latitudine,
+                Longitudine = criteri.Longitudine,
+                Distance = criteri.Distance,
+                Page = criteri.Page
+            };
+
+            return Ok(this.handler.Handle(query));
+        }
+    }
+}
